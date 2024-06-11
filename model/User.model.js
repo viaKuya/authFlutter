f23b2afd096184e6d02cfbba7a0259aa9e246804
viaKuya <eva.jerome.bcp@gmail.com>
2024-06-12 06:01:20 +0800

    Create User.model.js

---
+++ model/User.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userAuthSchema = new mongoose.Schema({
+    firstname:{
+        type:String,
+        required:true
+    },
+    lastname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+const userAuth = mongoose.model("userAuthSchema",userAuthSchema)
+
+module.exports = userAuth
